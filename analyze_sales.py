5194a872344f6a2bad22fde929a666593c6ca140
Jesper Collste <jesper.collste@elev.ga.ntig.se>
2024-08-23 12:59:31 +0200

    Uppgift 2 klar

--- analyze_sales.py
+++ analyze_sales.py
@@ -1,9 +1,11 @@
 import csv
 import os
 import locale
+from collections import Counter
 
 def analyze_sales_data(filename):
     products = {}
+    quantities = Counter()  # To keep track of the number of times each product is sold
 
     with open(filename, 'r') as file:
         reader = csv.DictReader(file)
@@ -11,26 +13,30 @@ def analyze_sales_data(filename):
             product = row['Product']
             sales = float(row['Sales'])
             
+            # Track total sales per product
             if product in products:
                 products[product] += sales
             else:
                 products[product] = sales
+            
+            # Track the count of each product sold
+            quantities[product] += 1
     
-    #TODO: Hitta den mest sålda produkten (TIPS! Använd Counter från collections)
     
-    # Hitta den mest lukrativa produkten
+    most_sold_product = quantities.most_common(1)[0]  # (product, count)
+    
+   
     most_lucrative_product = max(products, key=products.get)
     
-    # Genomsnittlig försäljning per produkt
+
     average_sales = sum(products.values()) / len(products)
 
-    print(f"Mest sålda produkt: ??, Antal: ??")  #FIXME: Redovisa mest sålda produkt här
-    print(f"Mest lukrativa produkt: \"{most_lucrative_product}\" med försäljning på {locale.currency(products[most_lucrative_product],grouping=True)}")
+    print(f"Mest sålda produkt: \"{most_sold_product[0]}\", Antal: {most_sold_product[1]}")
+    print(f"Mest lukrativa produkt: \"{most_lucrative_product}\" med försäljning på {locale.currency(products[most_lucrative_product], grouping=True)}")
     print(f"Genomsnittlig försäljning per produkt: {locale.currency(average_sales, grouping=True)}")
 
 
-# Sätt språkinställning till svenska (Sverige) används för att skriva ut formaterad valuta
-locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')  
+locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')
 
 os.system('cls')
 analyze_sales_data('sales_data.csv')
